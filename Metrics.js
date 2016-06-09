d19c235f56c23e8adf19085794e03d7a7c6e6c96
Dan Sparacio <dsparaci@akamai.com>
2016-06-09 10:25:41 -0700

    Separated metics object into own object.

---
+++ Metrics.js
@@ -0,0 +1,31 @@
+Metrics = function() {
+
+    var dict = {};
+
+    function createSession(info) {
+        dict[info.id] = {metrics:[], sessionInfo:info};
+    }
+
+    function push(id, set) {
+        dict[id].metrics.push(set);
+    }
+
+    function getSessions(id, set) {
+        return dict;
+    }
+
+    function getSessionById(id) {
+        return dict[id];
+    }
+
+    return {
+        push: push,
+        createSession: createSession,
+        getSessions: getSessions,
+        getSessionById: getSessionById
+    }
+}
+
+Metrics.prototype = {
+    constructor: Metrics
+};
